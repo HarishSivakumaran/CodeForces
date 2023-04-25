2c8960b193d906d20aa81c122ac4a07868deba4c
Harish Sivakumaran <harish.sivakumaran@sap.com>
2023-04-26 00:00:42 +0530

    1749C - Number Game

---
+++ Codeforces Round 867 (Div. 3)/1749C -NumberGame.java
@@ -0,0 +1,157 @@
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class Main {
+    
+    static  PrintWriter printWriter  = null;
+    
+
+    public static void main(String[] args){
+        
+        FastScanner in = new FastScanner(System.in);
+        printWriter = new PrintWriter(System.out);
+        int cases = in.nextInt();
+        
+        for(int z = 0; z < cases; z++){
+            int n = in.nextInt();
+            int[] arr = new int[n];
+            for(int i = 0; i < n; i++) arr[i] = in.nextInt();
+            Arrays.sort(arr);
+            
+            printWriter.printf(solve(arr, n) + "\n");
+        }
+        
+        printWriter.close();
+
+        
+    }
+    
+    public static int solve(int[] arr, int n){
+        int k = 1;
+        for(; k <= n; k++){
+            int l = 0, r = findNextSmaller(arr, k);
+            int i = k;
+            for(; i >= 1; i--){
+                while(r >= 0 && arr[r] > i) r--;
+                if(l > r ) return k-1;
+                l++;
+                r--;
+            }
+        }
+        
+        return k-1;
+        
+    }
+    
+    public static int findNextSmaller(int[] arr, int t){
+        int l = 0, r = arr.length-1;
+        
+        while(l <= r){
+            int m = l + (r-l)/2;
+            if(arr[m] == t) return m;
+            if(arr[m] > t){
+                r = m-1;
+            }else{
+                l = m+1;
+            }
+        }
+        
+        return r;
+    }
+        
+   public static void solve(int[] original, int[] partSorted){
+       
+      Stack<int[]> stack = new Stack();
+      int l = 0, r = 0;
+      
+    boolean misMatch = false;
+      
+      for(int i = 0; i < partSorted.length; i++){
+          if(stack.size() > 0 && partSorted[i] < stack.peek()[1]){
+              if(misMatch && stack.size() > r-l+1){
+                  r = stack.peek()[0] + 1;
+                  l = r - stack.size() + 1;
+              }
+              stack.clear();
+              misMatch = false;
+          }
+          if(original[i] != partSorted[i]) misMatch = true;
+          stack.push(new int[]{i, partSorted[i]});
+      }
+      
+        if(misMatch && stack.size() > r-l+1){
+                  r = stack.peek()[0] + 1;
+                  l = r - stack.size() + 1;
+        }
+
+      printWriter.printf(l+" " + r + "\n");
+        
+   }
+    
+    
+    
+public static class FastScanner {
+    private BufferedReader reader = null;
+    private StringTokenizer tokenizer = null;
+
+    public FastScanner(InputStream in) {
+        reader = new BufferedReader(new InputStreamReader(in));
+        tokenizer = null;
+    }
+
+    public String next() {
+        if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+
+    public String nextLine() {
+        if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                return reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return tokenizer.nextToken("\n");
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+     
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+    
+    public double nextDouble() {
+    	 return Double.parseDouble(next());
+	 }
+    
+    public int[] nextIntArray(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = nextInt();
+        return a;
+    }
+    
+    public long[] nextLongArray(int n) {
+        long[] a = new long[n];
+        for (int i = 0; i < n; i++)
+            a[i] = nextLong();
+        return a;
+    } 
+}
+
+}
+
